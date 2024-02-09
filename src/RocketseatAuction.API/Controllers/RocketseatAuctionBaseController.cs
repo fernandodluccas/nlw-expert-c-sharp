ace76d27d4caaf98cf5978ae20abb972dc3c385b
Fernando D'luccas <dluccash@hotmail.com>
2024-02-09 18:21:34 -0300

    Aula 02

---
+++ src/RocketseatAuction.API/Controllers/RocketseatAuctionBaseController.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RocketseatAuction.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class RocketseatAuctionBaseController : ControllerBase
+    {
+    }
+}
